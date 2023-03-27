586125536819a3671d2bc14acb2175ed1d06652a
hieu <hieuact13011998@gmail.com>
2023-03-27 20:42:00 +0700

    update

--- user-service/src/handler.ts
+++ user-service/src/handler.ts
@@ -1,15 +1,16 @@
-import { APIGatewayProxyEventV2 } from "aws-lambda";
+import { APIGatewayProxyEventV2, APIGatewayProxyHandlerV2, APIGatewayProxyResultV2, Context } from "aws-lambda";
 
-export const Signup = (event: APIGatewayProxyEventV2) => {
-  console.log(event);
-
+export const Signup: APIGatewayProxyHandlerV2 = async (event: APIGatewayProxyEventV2, context: Context): Promise<APIGatewayProxyResultV2> => {
+  // // console.log(event);
+  // context.callbackWaitsForEmptyEventLoop = false;
+  // console.log("ENVIRONMENT VARIABLES\n" + JSON.stringify(process.env, null, 2))
+  // console.info("EVENT\n" + JSON.stringify(event, null, 2))
+  // console.warn("Event not processed.", context)
   return {
     statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      // "Content-Type": "text/html"
-    },
-    body: JSON.stringify({ message: 'Response for signup', data: { success: true } })
-  }
-
+    body: JSON.stringify({
+      message: "successfully",
+      data: []
+    })
+  };
 }
